aa8ede677b45e999856998c681b52544d0fdd98d
Rodgers Okwemba <rodgerstarrodgers@gmail.com>
2025-09-30 14:59:30 +0300

    Get requests

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const app = express();
 
+const courses = [
+    { id: 1, name: 'courses' },
+    { id: 2, name: 'courses2', teacher: 'ozy', subject: 'Philosophy' },
+    { id: 2, name: 'courses3' },
+]
+
 app.get('/', (req, res) => {
     res.send('Found Life Out Here!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Course Not Found.');
+    res.send(course);
 });
+
 
 
 // // Routes Params
@@ -20,9 +29,9 @@ app.get('/api/courses/:id', (req, res) => {
 // });
 
 // Query Parameters
-app.get('/api/post/:year/:month', (req, res) => {
-    res.send(req.query);
-});
+// app.get('/api/post/:year/:month', (req, res) => {
+//     res.send(req.query);
+// });
 
 
 //NB: Path/Routes Parameters: Use when the parameter is essential to identify the resource. 
